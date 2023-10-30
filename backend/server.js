d7a703dd79126036b4418b6584a3b36079f8a3d7
ShlokPrajapati <thehulk03sp@gmail.com>
2023-10-30 22:04:31 +0530

    add backend cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const morgan=require('morgan')
 const dotenv=require("dotenv")
+const cors=require("cors")
 const connectDB =require('./config/db')
 
 const app=express()
